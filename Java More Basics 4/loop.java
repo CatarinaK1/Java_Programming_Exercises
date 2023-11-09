44bfbec8b4b8439b458ae14fd11fe7361f859adf
Catarina Kaucher <111956032+CatarinaK1@users.noreply.github.com>
2023-11-09 13:58:05 +0200

    Create loop.java

---
+++ Java More Basics 4/loop.java
@@ -0,0 +1,13 @@
+package test;
+
+public class loop {
+
+	public static void main(String[] args) {
+		String dd = "loop";
+		int count = 0;
+				while (count< 5) {
+					System.out.println(dd);
+					count++;
+				}
+	}
+}
