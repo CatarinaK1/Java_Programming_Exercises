1d14e765c6c858f3d48bc599c80943ae131658cc
Catarina Kaucher <111956032+CatarinaK1@users.noreply.github.com>
2023-11-09 13:40:39 +0200

    Create age.java

---
+++ First_Java_Program/age.java
@@ -0,0 +1,15 @@
+public class age {
+	public static void main(String[] args) {
+
+		int age = 10;
+		if (age > 0 && age < 18) {
+			System.out.println("You are underage.");
+		}
+
+		else {
+			System.out.println("You are an adult.");
+		}
+
+		System.out.println("Press space to exit.");
+	}
+}
